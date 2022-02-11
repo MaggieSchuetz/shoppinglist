e716227ca77a3ea02b923bea80c965f887e2386f
Magdalena Schuetz <m.schuetz@outlook.de>
2022-02-11 12:48:49 +0100

    Add fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    loadItems();
+  }, []);
+
+  async function loadItems() {
+    try {
+      const response = await fetch(
+        'https://fetch-me.vercel.app/api/shopping/items'
+      );
+      if (response.ok) {
+        const results = await response.json();
+        setItems(results.data);
+        console.log(items);
+      } else {
+        throw new Error('Error: 404 not found');
+      }
+    } catch (error) {
+      setHasError(true);
+    }
+  }
   return (
     <AppGrid>
       <h1>Shopping List</h1>
+
+      {hasError && <p>Error: could not load shopping items</p>}
     </AppGrid>
   );
 }
