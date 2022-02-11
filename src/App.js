7889174d53f75b1bf5e4a064dbbd0e2127e3e920
Magdalena Schuetz <m.schuetz@outlook.de>
2022-02-11 11:53:29 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppGrid>
+      <h1>Shopping List</h1>
+    </AppGrid>
   );
 }
 
 export default App;
+
+const AppGrid = styled.main`
+  display: grid;
+  gap: 5px;
+`;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing:border-box;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  background: linear-gradient(to right, #D4D3DD, #EFEFBB);
+}
+
+`;
+export default GlobalStyle;
